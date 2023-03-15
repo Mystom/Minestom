ec6152ea08cfe81794469241b8b6c56a65ed0fef
devPesto <pestonotpasta@gmail.com>
2023-03-15 12:35:57 +0300

    Simplify placement rules

--- src/main/java/net/minestom/server/extras/PlacementRules.java
+++ src/main/java/net/minestom/server/extras/PlacementRules.java
@@ -3,10 +3,7 @@ package net.minestom.server.extras;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockManager;
-import net.minestom.server.instance.block.rule.vanilla.AxisPlacementRule;
-import net.minestom.server.instance.block.rule.vanilla.RedstonePlacementRule;
-import net.minestom.server.instance.block.rule.vanilla.StairsPlacementRule;
-import net.minestom.server.instance.block.rule.vanilla.WallPlacementRule;
+import net.minestom.server.instance.block.rule.vanilla.*;
 
 public final class PlacementRules {
 
@@ -18,55 +15,38 @@ public final class PlacementRules {
      * - redstone dust
      */
     public static void init() {
-        initAxisBlocks();
-        initStairs();
-        initWalls();
-        initRedstone();
-    }
-
-    /**
-     * Register all the blocks that support axis placement (logs, hay block, etc)
-     * using {@link AxisPlacementRule}.
-     */
-    public static void initAxisBlocks() {
-        BlockManager blockManager = MinecraftServer.getBlockManager();
-        for (Block block : Block.values()) {
-            if (block.getProperty("axis") != null) {
-                blockManager.registerBlockPlacementRule(new AxisPlacementRule(block));
-            }
-        }
-    }
-
-    /**
-     * Register all the stairs blocks using {@link StairsPlacementRule}.
-     */
-    public static void initStairs() {
-        BlockManager blockManager = MinecraftServer.getBlockManager();
-        for (Block block : Block.values()) {
-            if (block.namespace().value().endsWith("stairs")) {
-                blockManager.registerBlockPlacementRule(new StairsPlacementRule(block));
-            }
-        }
-    }
-
-    /**
-     * Register all the wall blocks using {@link WallPlacementRule}.
-     */
-    public static void initWalls() {
-        BlockManager blockManager = MinecraftServer.getBlockManager();
+        BlockManager manager = MinecraftServer.getBlockManager();
         for (Block block : Block.values()) {
             String namespace = block.namespace().value();
-            if (namespace.endsWith("wall")) {
-                blockManager.registerBlockPlacementRule(new WallPlacementRule(block));
-            }
-        }
-    }
 
-    /**
-     * Register the {@link RedstonePlacementRule}.
-     */
-    public static void initRedstone() {
-        BlockManager blockManager = MinecraftServer.getBlockManager();
-        blockManager.registerBlockPlacementRule(new RedstonePlacementRule());
+            // Stairs
+            if (namespace.endsWith("stairs")) {
+                manager.registerBlockPlacementRule(new StairsPlacementRule(block));
+                continue;
+            }
+
+            // Walls
+            if (namespace.endsWith("wall")) {
+                manager.registerBlockPlacementRule(new WallPlacementRule(block));
+                continue;
+            }
+
+            // Slabs
+            if (namespace.endsWith("slab")){
+                manager.registerBlockPlacementRule(new SlabPlacementRule(block));
+                continue;
+            }
+
+            // Axis - Hay bales, logs, etc
+            if (block.getProperty("axis") != null) {
+                manager.registerBlockPlacementRule(new AxisPlacementRule(block));
+                continue;
+            }
+
+            if (block.isSolid())
+                manager.registerBlockPlacementRule(new DefaultPlacementRule(block));
+        }
+
+        manager.registerBlockPlacementRule(new RedstonePlacementRule());
     }
 }
